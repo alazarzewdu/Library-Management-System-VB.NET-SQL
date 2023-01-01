b2945c9f8f03c6363ba6867e86b2a021526df057
Alazar Zewdu <alazarzewdu3@gmail.com>
2023-01-01 00:19:31 -0800

    Finished designing the layout

--- Library-MANAGEMENT/Library-MANAGEMENT/Form1.Designer.vb
+++ Library-MANAGEMENT/Library-MANAGEMENT/Form1.Designer.vb
@@ -22,9 +22,386 @@ Partial Class Form1
     'Do not modify it using the code editor.
     <System.Diagnostics.DebuggerStepThrough()> _
     Private Sub InitializeComponent()
-        components = New System.ComponentModel.Container()
+        Dim resources As System.ComponentModel.ComponentResourceManager = New System.ComponentModel.ComponentResourceManager(GetType(Form1))
+        Me.GroupBox1 = New System.Windows.Forms.GroupBox()
+        Me.TextBoxShelf = New System.Windows.Forms.TextBox()
+        Me.Label5 = New System.Windows.Forms.Label()
+        Me.TextBoxAuthor = New System.Windows.Forms.TextBox()
+        Me.Label4 = New System.Windows.Forms.Label()
+        Me.TextBoxName = New System.Windows.Forms.TextBox()
+        Me.Label3 = New System.Windows.Forms.Label()
+        Me.TextBoxId = New System.Windows.Forms.TextBox()
+        Me.Label2 = New System.Windows.Forms.Label()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.GroupBox2 = New System.Windows.Forms.GroupBox()
+        Me.Label6 = New System.Windows.Forms.Label()
+        Me.TextBox1 = New System.Windows.Forms.TextBox()
+        Me.CheckBoxById = New System.Windows.Forms.CheckBox()
+        Me.CheckBoxByName = New System.Windows.Forms.CheckBox()
+        Me.CheckBoxByAuthor = New System.Windows.Forms.CheckBox()
+        Me.DataGridView1 = New System.Windows.Forms.DataGridView()
+        Me.Label7 = New System.Windows.Forms.Label()
+        Me.ButtonClearSelectedandImagePreview = New System.Windows.Forms.Button()
+        Me.PictureBoxImagePreview = New System.Windows.Forms.PictureBox()
+        Me.ButtonRefresh = New System.Windows.Forms.Button()
+        Me.ButtonDelete = New System.Windows.Forms.Button()
+        Me.ButtonEdit = New System.Windows.Forms.Button()
+        Me.ButtonClearAll = New System.Windows.Forms.Button()
+        Me.ButtonSave = New System.Windows.Forms.Button()
+        Me.ButtonIdMaker = New System.Windows.Forms.Button()
+        Me.PictureBoxImageInput = New System.Windows.Forms.PictureBox()
+        Me.GroupBox1.SuspendLayout()
+        Me.GroupBox2.SuspendLayout()
+        CType(Me.DataGridView1, System.ComponentModel.ISupportInitialize).BeginInit()
+        CType(Me.PictureBoxImagePreview, System.ComponentModel.ISupportInitialize).BeginInit()
+        CType(Me.PictureBoxImageInput, System.ComponentModel.ISupportInitialize).BeginInit()
+        Me.SuspendLayout()
+        '
+        'GroupBox1
+        '
+        Me.GroupBox1.Controls.Add(Me.ButtonClearAll)
+        Me.GroupBox1.Controls.Add(Me.ButtonSave)
+        Me.GroupBox1.Controls.Add(Me.ButtonIdMaker)
+        Me.GroupBox1.Controls.Add(Me.TextBoxShelf)
+        Me.GroupBox1.Controls.Add(Me.Label5)
+        Me.GroupBox1.Controls.Add(Me.TextBoxAuthor)
+        Me.GroupBox1.Controls.Add(Me.Label4)
+        Me.GroupBox1.Controls.Add(Me.TextBoxName)
+        Me.GroupBox1.Controls.Add(Me.Label3)
+        Me.GroupBox1.Controls.Add(Me.TextBoxId)
+        Me.GroupBox1.Controls.Add(Me.Label2)
+        Me.GroupBox1.Controls.Add(Me.Label1)
+        Me.GroupBox1.Controls.Add(Me.PictureBoxImageInput)
+        Me.GroupBox1.Location = New System.Drawing.Point(12, 12)
+        Me.GroupBox1.Name = "GroupBox1"
+        Me.GroupBox1.Size = New System.Drawing.Size(768, 235)
+        Me.GroupBox1.TabIndex = 0
+        Me.GroupBox1.TabStop = False
+        Me.GroupBox1.Text = "GroupBox1"
+        '
+        'TextBoxShelf
+        '
+        Me.TextBoxShelf.Anchor = CType(((System.Windows.Forms.AnchorStyles.Top Or System.Windows.Forms.AnchorStyles.Left) _
+            Or System.Windows.Forms.AnchorStyles.Right), System.Windows.Forms.AnchorStyles)
+        Me.TextBoxShelf.Location = New System.Drawing.Point(43, 131)
+        Me.TextBoxShelf.Name = "TextBoxShelf"
+        Me.TextBoxShelf.Size = New System.Drawing.Size(192, 20)
+        Me.TextBoxShelf.TabIndex = 9
+        '
+        'Label5
+        '
+        Me.Label5.AutoSize = True
+        Me.Label5.Location = New System.Drawing.Point(2, 137)
+        Me.Label5.Name = "Label5"
+        Me.Label5.RightToLeft = System.Windows.Forms.RightToLeft.No
+        Me.Label5.Size = New System.Drawing.Size(31, 13)
+        Me.Label5.TabIndex = 8
+        Me.Label5.Text = "Shelf"
+        '
+        'TextBoxAuthor
+        '
+        Me.TextBoxAuthor.Anchor = CType(((System.Windows.Forms.AnchorStyles.Top Or System.Windows.Forms.AnchorStyles.Left) _
+            Or System.Windows.Forms.AnchorStyles.Right), System.Windows.Forms.AnchorStyles)
+        Me.TextBoxAuthor.Location = New System.Drawing.Point(43, 96)
+        Me.TextBoxAuthor.Name = "TextBoxAuthor"
+        Me.TextBoxAuthor.Size = New System.Drawing.Size(192, 20)
+        Me.TextBoxAuthor.TabIndex = 7
+        '
+        'Label4
+        '
+        Me.Label4.AutoSize = True
+        Me.Label4.Location = New System.Drawing.Point(2, 99)
+        Me.Label4.Name = "Label4"
+        Me.Label4.RightToLeft = System.Windows.Forms.RightToLeft.No
+        Me.Label4.Size = New System.Drawing.Size(38, 13)
+        Me.Label4.TabIndex = 6
+        Me.Label4.Text = "Author"
+        '
+        'TextBoxName
+        '
+        Me.TextBoxName.Anchor = CType(((System.Windows.Forms.AnchorStyles.Top Or System.Windows.Forms.AnchorStyles.Left) _
+            Or System.Windows.Forms.AnchorStyles.Right), System.Windows.Forms.AnchorStyles)
+        Me.TextBoxName.Location = New System.Drawing.Point(43, 62)
+        Me.TextBoxName.Name = "TextBoxName"
+        Me.TextBoxName.Size = New System.Drawing.Size(192, 20)
+        Me.TextBoxName.TabIndex = 5
+        '
+        'Label3
+        '
+        Me.Label3.AutoSize = True
+        Me.Label3.Location = New System.Drawing.Point(2, 65)
+        Me.Label3.Name = "Label3"
+        Me.Label3.Size = New System.Drawing.Size(35, 13)
+        Me.Label3.TabIndex = 4
+        Me.Label3.Text = "Name"
+        '
+        'TextBoxId
+        '
+        Me.TextBoxId.Anchor = CType(((System.Windows.Forms.AnchorStyles.Top Or System.Windows.Forms.AnchorStyles.Left) _
+            Or System.Windows.Forms.AnchorStyles.Right), System.Windows.Forms.AnchorStyles)
+        Me.TextBoxId.Location = New System.Drawing.Point(43, 29)
+        Me.TextBoxId.Name = "TextBoxId"
+        Me.TextBoxId.Size = New System.Drawing.Size(190, 20)
+        Me.TextBoxId.TabIndex = 3
+        '
+        'Label2
+        '
+        Me.Label2.AutoSize = True
+        Me.Label2.Location = New System.Drawing.Point(6, 32)
+        Me.Label2.Name = "Label2"
+        Me.Label2.Size = New System.Drawing.Size(16, 13)
+        Me.Label2.TabIndex = 2
+        Me.Label2.Text = "Id"
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Location = New System.Drawing.Point(546, 32)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(36, 13)
+        Me.Label1.TabIndex = 1
+        Me.Label1.Text = "Image"
+        '
+        'GroupBox2
+        '
+        Me.GroupBox2.Controls.Add(Me.ButtonClearSelectedandImagePreview)
+        Me.GroupBox2.Controls.Add(Me.Label7)
+        Me.GroupBox2.Controls.Add(Me.DataGridView1)
+        Me.GroupBox2.Controls.Add(Me.PictureBoxImagePreview)
+        Me.GroupBox2.Controls.Add(Me.CheckBoxByAuthor)
+        Me.GroupBox2.Controls.Add(Me.CheckBoxByName)
+        Me.GroupBox2.Controls.Add(Me.CheckBoxById)
+        Me.GroupBox2.Controls.Add(Me.TextBox1)
+        Me.GroupBox2.Controls.Add(Me.Label6)
+        Me.GroupBox2.Controls.Add(Me.ButtonRefresh)
+        Me.GroupBox2.Controls.Add(Me.ButtonDelete)
+        Me.GroupBox2.Controls.Add(Me.ButtonEdit)
+        Me.GroupBox2.Location = New System.Drawing.Point(12, 253)
+        Me.GroupBox2.Name = "GroupBox2"
+        Me.GroupBox2.Size = New System.Drawing.Size(768, 364)
+        Me.GroupBox2.TabIndex = 1
+        Me.GroupBox2.TabStop = False
+        Me.GroupBox2.Text = "GroupBox2"
+        '
+        'Label6
+        '
+        Me.Label6.AutoSize = True
+        Me.Label6.Location = New System.Drawing.Point(270, 19)
+        Me.Label6.Name = "Label6"
+        Me.Label6.RightToLeft = System.Windows.Forms.RightToLeft.No
+        Me.Label6.Size = New System.Drawing.Size(44, 13)
+        Me.Label6.TabIndex = 9
+        Me.Label6.Text = "Search:"
+        '
+        'TextBox1
+        '
+        Me.TextBox1.Location = New System.Drawing.Point(320, 16)
+        Me.TextBox1.Name = "TextBox1"
+        Me.TextBox1.Size = New System.Drawing.Size(204, 20)
+        Me.TextBox1.TabIndex = 10
+        '
+        'CheckBoxById
+        '
+        Me.CheckBoxById.AutoSize = True
+        Me.CheckBoxById.Location = New System.Drawing.Point(320, 42)
+        Me.CheckBoxById.Name = "CheckBoxById"
+        Me.CheckBoxById.Size = New System.Drawing.Size(37, 17)
+        Me.CheckBoxById.TabIndex = 11
+        Me.CheckBoxById.Text = "ID"
+        Me.CheckBoxById.UseVisualStyleBackColor = True
+        '
+        'CheckBoxByName
+        '
+        Me.CheckBoxByName.AutoSize = True
+        Me.CheckBoxByName.Location = New System.Drawing.Point(363, 42)
+        Me.CheckBoxByName.Name = "CheckBoxByName"
+        Me.CheckBoxByName.Size = New System.Drawing.Size(54, 17)
+        Me.CheckBoxByName.TabIndex = 12
+        Me.CheckBoxByName.Text = "Name"
+        Me.CheckBoxByName.UseVisualStyleBackColor = True
+        '
+        'CheckBoxByAuthor
+        '
+        Me.CheckBoxByAuthor.AutoSize = True
+        Me.CheckBoxByAuthor.Location = New System.Drawing.Point(423, 42)
+        Me.CheckBoxByAuthor.Name = "CheckBoxByAuthor"
+        Me.CheckBoxByAuthor.Size = New System.Drawing.Size(57, 17)
+        Me.CheckBoxByAuthor.TabIndex = 13
+        Me.CheckBoxByAuthor.Text = "Author"
+        Me.CheckBoxByAuthor.UseVisualStyleBackColor = True
+        '
+        'DataGridView1
+        '
+        Me.DataGridView1.AllowUserToAddRows = False
+        Me.DataGridView1.AllowUserToDeleteRows = False
+        Me.DataGridView1.AutoSizeColumnsMode = System.Windows.Forms.DataGridViewAutoSizeColumnsMode.Fill
+        Me.DataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize
+        Me.DataGridView1.Location = New System.Drawing.Point(9, 68)
+        Me.DataGridView1.Name = "DataGridView1"
+        Me.DataGridView1.ReadOnly = True
+        Me.DataGridView1.SelectionMode = System.Windows.Forms.DataGridViewSelectionMode.FullRowSelect
+        Me.DataGridView1.Size = New System.Drawing.Size(536, 287)
+        Me.DataGridView1.TabIndex = 14
+        '
+        'Label7
+        '
+        Me.Label7.AutoSize = True
+        Me.Label7.Location = New System.Drawing.Point(551, 68)
+        Me.Label7.Name = "Label7"
+        Me.Label7.RightToLeft = System.Windows.Forms.RightToLeft.No
+        Me.Label7.Size = New System.Drawing.Size(36, 13)
+        Me.Label7.TabIndex = 15
+        Me.Label7.Text = "Image"
+        '
+        'ButtonClearSelectedandImagePreview
+        '
+        Me.ButtonClearSelectedandImagePreview.Location = New System.Drawing.Point(588, 259)
+        Me.ButtonClearSelectedandImagePreview.Name = "ButtonClearSelectedandImagePreview"
+        Me.ButtonClearSelectedandImagePreview.Size = New System.Drawing.Size(174, 23)
+        Me.ButtonClearSelectedandImagePreview.TabIndex = 16
+        Me.ButtonClearSelectedandImagePreview.Text = "Clear Selected and Image"
+        Me.ButtonClearSelectedandImagePreview.UseVisualStyleBackColor = True
+        '
+        'PictureBoxImagePreview
+        '
+        Me.PictureBoxImagePreview.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle
+        Me.PictureBoxImagePreview.Location = New System.Drawing.Point(588, 68)
+        Me.PictureBoxImagePreview.Name = "PictureBoxImagePreview"
+        Me.PictureBoxImagePreview.Size = New System.Drawing.Size(174, 174)
+        Me.PictureBoxImagePreview.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom
+        Me.PictureBoxImagePreview.TabIndex = 13
+        Me.PictureBoxImagePreview.TabStop = False
+        '
+        'ButtonRefresh
+        '
+        Me.ButtonRefresh.Image = Global.Library_MANAGEMENT.My.Resources.Resources.refresh
+        Me.ButtonRefresh.ImageAlign = System.Drawing.ContentAlignment.MiddleLeft
+        Me.ButtonRefresh.Location = New System.Drawing.Point(182, 19)
+        Me.ButtonRefresh.Name = "ButtonRefresh"
+        Me.ButtonRefresh.Size = New System.Drawing.Size(82, 40)
+        Me.ButtonRefresh.TabIndex = 2
+        Me.ButtonRefresh.Text = "Refresh"
+        Me.ButtonRefresh.TextAlign = System.Drawing.ContentAlignment.MiddleRight
+        Me.ButtonRefresh.UseVisualStyleBackColor = True
+        '
+        'ButtonDelete
+        '
+        Me.ButtonDelete.Image = Global.Library_MANAGEMENT.My.Resources.Resources.delete
+        Me.ButtonDelete.ImageAlign = System.Drawing.ContentAlignment.MiddleLeft
+        Me.ButtonDelete.Location = New System.Drawing.Point(94, 19)
+        Me.ButtonDelete.Name = "ButtonDelete"
+        Me.ButtonDelete.Size = New System.Drawing.Size(82, 40)
+        Me.ButtonDelete.TabIndex = 1
+        Me.ButtonDelete.Text = "Delete"
+        Me.ButtonDelete.TextAlign = System.Drawing.ContentAlignment.MiddleRight
+        Me.ButtonDelete.UseVisualStyleBackColor = True
+        '
+        'ButtonEdit
+        '
+        Me.ButtonEdit.Image = Global.Library_MANAGEMENT.My.Resources.Resources.e
+        Me.ButtonEdit.ImageAlign = System.Drawing.ContentAlignment.MiddleLeft
+        Me.ButtonEdit.Location = New System.Drawing.Point(9, 19)
+        Me.ButtonEdit.Name = "ButtonEdit"
+        Me.ButtonEdit.Size = New System.Drawing.Size(82, 40)
+        Me.ButtonEdit.TabIndex = 0
+        Me.ButtonEdit.Text = "Edit"
+        Me.ButtonEdit.TextAlign = System.Drawing.ContentAlignment.MiddleRight
+        Me.ButtonEdit.UseVisualStyleBackColor = True
+        '
+        'ButtonClearAll
+        '
+        Me.ButtonClearAll.Image = Global.Library_MANAGEMENT.My.Resources.Resources.clear1
+        Me.ButtonClearAll.ImageAlign = System.Drawing.ContentAlignment.MiddleLeft
+        Me.ButtonClearAll.Location = New System.Drawing.Point(119, 186)
+        Me.ButtonClearAll.Name = "ButtonClearAll"
+        Me.ButtonClearAll.Size = New System.Drawing.Size(65, 43)
+        Me.ButtonClearAll.TabIndex = 12
+        Me.ButtonClearAll.Text = "Clear"
+        Me.ButtonClearAll.TextAlign = System.Drawing.ContentAlignment.MiddleRight
+        Me.ButtonClearAll.UseVisualStyleBackColor = True
+        '
+        'ButtonSave
+        '
+        Me.ButtonSave.Image = Global.Library_MANAGEMENT.My.Resources.Resources.save
+        Me.ButtonSave.ImageAlign = System.Drawing.ContentAlignment.MiddleLeft
+        Me.ButtonSave.Location = New System.Drawing.Point(9, 186)
+        Me.ButtonSave.Name = "ButtonSave"
+        Me.ButtonSave.Size = New System.Drawing.Size(65, 43)
+        Me.ButtonSave.TabIndex = 11
+        Me.ButtonSave.Text = "Save"
+        Me.ButtonSave.TextAlign = System.Drawing.ContentAlignment.MiddleRight
+        Me.ButtonSave.UseVisualStyleBackColor = True
+        '
+        'ButtonIdMaker
+        '
+        Me.ButtonIdMaker.Image = CType(resources.GetObject("ButtonIdMaker.Image"), System.Drawing.Image)
+        Me.ButtonIdMaker.ImageAlign = System.Drawing.ContentAlignment.MiddleLeft
+        Me.ButtonIdMaker.Location = New System.Drawing.Point(245, 27)
+        Me.ButtonIdMaker.Name = "ButtonIdMaker"
+        Me.ButtonIdMaker.Size = New System.Drawing.Size(92, 22)
+        Me.ButtonIdMaker.TabIndex = 10
+        Me.ButtonIdMaker.Text = "Generate"
+        Me.ButtonIdMaker.TextAlign = System.Drawing.ContentAlignment.MiddleRight
+        Me.ButtonIdMaker.UseVisualStyleBackColor = True
+        '
+        'PictureBoxImageInput
+        '
+        Me.PictureBoxImageInput.Image = Global.Library_MANAGEMENT.My.Resources.Resources.image1
+        Me.PictureBoxImageInput.Location = New System.Drawing.Point(588, 19)
+        Me.PictureBoxImageInput.Name = "PictureBoxImageInput"
+        Me.PictureBoxImageInput.Size = New System.Drawing.Size(174, 174)
+        Me.PictureBoxImageInput.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom
+        Me.PictureBoxImageInput.TabIndex = 0
+        Me.PictureBoxImageInput.TabStop = False
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
         Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
-        Me.Text = "Form1"
+        Me.BackColor = System.Drawing.Color.White
+        Me.ClientSize = New System.Drawing.Size(792, 620)
+        Me.Controls.Add(Me.GroupBox2)
+        Me.Controls.Add(Me.GroupBox1)
+        Me.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle
+        Me.Name = "Form1"
+        Me.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen
+        Me.Text = "Unity University Library"
+        Me.GroupBox1.ResumeLayout(False)
+        Me.GroupBox1.PerformLayout()
+        Me.GroupBox2.ResumeLayout(False)
+        Me.GroupBox2.PerformLayout()
+        CType(Me.DataGridView1, System.ComponentModel.ISupportInitialize).EndInit()
+        CType(Me.PictureBoxImagePreview, System.ComponentModel.ISupportInitialize).EndInit()
+        CType(Me.PictureBoxImageInput, System.ComponentModel.ISupportInitialize).EndInit()
+        Me.ResumeLayout(False)
+
     End Sub
+    Friend WithEvents GroupBox1 As System.Windows.Forms.GroupBox
+    Friend WithEvents PictureBoxImageInput As System.Windows.Forms.PictureBox
+    Friend WithEvents ButtonClearAll As System.Windows.Forms.Button
+    Friend WithEvents ButtonSave As System.Windows.Forms.Button
+    Friend WithEvents ButtonIdMaker As System.Windows.Forms.Button
+    Friend WithEvents TextBoxShelf As System.Windows.Forms.TextBox
+    Friend WithEvents Label5 As System.Windows.Forms.Label
+    Friend WithEvents TextBoxAuthor As System.Windows.Forms.TextBox
+    Friend WithEvents Label4 As System.Windows.Forms.Label
+    Friend WithEvents TextBoxName As System.Windows.Forms.TextBox
+    Friend WithEvents Label3 As System.Windows.Forms.Label
+    Friend WithEvents TextBoxId As System.Windows.Forms.TextBox
+    Friend WithEvents Label2 As System.Windows.Forms.Label
+    Friend WithEvents Label1 As System.Windows.Forms.Label
+    Friend WithEvents GroupBox2 As System.Windows.Forms.GroupBox
+    Friend WithEvents ButtonRefresh As System.Windows.Forms.Button
+    Friend WithEvents ButtonDelete As System.Windows.Forms.Button
+    Friend WithEvents ButtonEdit As System.Windows.Forms.Button
+    Friend WithEvents DataGridView1 As System.Windows.Forms.DataGridView
+    Friend WithEvents PictureBoxImagePreview As System.Windows.Forms.PictureBox
+    Friend WithEvents CheckBoxByAuthor As System.Windows.Forms.CheckBox
+    Friend WithEvents CheckBoxByName As System.Windows.Forms.CheckBox
+    Friend WithEvents CheckBoxById As System.Windows.Forms.CheckBox
+    Friend WithEvents TextBox1 As System.Windows.Forms.TextBox
+    Friend WithEvents Label6 As System.Windows.Forms.Label
+    Friend WithEvents Label7 As System.Windows.Forms.Label
+    Friend WithEvents ButtonClearSelectedandImagePreview As System.Windows.Forms.Button
 
 End Class
--- Library-MANAGEMENT/Library-MANAGEMENT/Form1.vb
+++ Library-MANAGEMENT/Library-MANAGEMENT/Form1.vb
@@ -1,3 +1,10 @@
 ﻿Public Class Form1
 
+    Private Sub PictureBox1_Click(sender As Object, e As EventArgs) Handles PictureBoxImageInput.Click
+
+    End Sub
+
+    Private Sub ButtonSave_Click(sender As Object, e As EventArgs) Handles ButtonSave.Click
+
+    End Sub
 End Class
