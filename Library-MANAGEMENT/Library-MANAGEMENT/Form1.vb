f7f03295924f1a9bc06d7efe87f238147a63d6fb
Alazar Zewdu <alazarzewdu3@gmail.com>
2023-01-01 00:27:12 -0800

    form load

--- Library-MANAGEMENT/Library-MANAGEMENT/Form1.vb
+++ Library-MANAGEMENT/Library-MANAGEMENT/Form1.vb
@@ -7,4 +7,26 @@
     Private Sub ButtonSave_Click(sender As Object, e As EventArgs) Handles ButtonSave.Click
 
     End Sub
+
+    Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        Me.CenterToScreen()
+        ShowData()
+    End Sub
+
+
+    Private Sub ShowData()
+        Try
+            ConnectionState.Open()
+        Catch ex As Exception
+            MessageBox.Show("Connection Failed !" & vbCrLf & "Please check that the server is running", "Error Message".Contains MessafeBoxButtons.OK, MessageBoxIcon.Error)
+            Return
+        End Try
+
+        Try
+           
+            End If
+        Catch ex As Exception
+
+        End Try
+    End Sub
 End Class
